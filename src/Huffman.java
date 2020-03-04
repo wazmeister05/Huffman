1d0788e07806717628fc3bada15150acc09ee637
wazmeister05 <w.stolton@gmail.com>
2020-03-04 14:22:02 +0000

    Initial commit

---
+++ src/Huffman.java
@@ -0,0 +1,70 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Huffman {
+
+    public static void printCode(HuffmanNode root, String s){
+        if(root.left == null && root.right == null && Character.isLetter(root.c)){
+            System.out.println(root.c + ":" + s);
+            return;
+        }
+
+        printCode(root.left, s + "0");
+        printCode(root.right, s + "1");
+    }
+
+    public static class HuffmanNode{
+        int data;
+        char c;
+
+        HuffmanNode left;
+        HuffmanNode right;
+    }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        String text = "Batman";
+        int n = text.length();
+        char[] charArray = text.toCharArray();
+        int[] charFreq = {1,2,1,1,2,1};         //Bx1 Ax2 Tx1 Mx1 Ax2 Nx1
+
+        PriorityQueue<HuffmanNode> queue = new PriorityQueue<>(n, new MyComparator());
+
+        for(int i = 0; i < n; i++){
+            HuffmanNode hn = new HuffmanNode();
+            hn.c = charArray[i];
+            hn.data = charFreq[i];
+
+            hn.left = null;
+            hn.right = null;
+
+            queue.add(hn);
+        }
+
+        HuffmanNode root = null;
+
+        while(queue.size() > 1){
+            HuffmanNode x = queue.peek();
+            queue.poll();
+            HuffmanNode y = queue.peek();
+            queue.poll();
+            HuffmanNode f = new HuffmanNode();
+            f.data = x.data + y.data;       //freq at which the characters occur
+
+            f.c = '-';
+            f.left = x;
+            f.right = y;
+            root = f;
+            queue.add(f);
+        }
+        printCode(root, "");
+    }
+
+    static class MyComparator implements Comparator<HuffmanNode> {
+        public int compare(HuffmanNode x, HuffmanNode y){
+            return x.data - y.data;
+        }
+    }
+}
